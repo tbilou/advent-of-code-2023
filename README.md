f8aaf0b1184bc4ac16817d58e50907988ebaf482
tiago bilou <tiago.bilou@xing.com>
2023-12-01 17:19:16 +0100

    solution for day 1

--- README.md
+++ README.md
@@ -16,6 +16,14 @@ If you're stuck with Kotlin-specific questions or anything related to this templ
     Every year since then, beginning on the first day of December, a programming puzzle is published every day for twenty-five days.
     You can solve the puzzle and provide an answer using the language of your choice.
 
+| Day | success?   | Notes                                                                                                                                                                                                                                                                                                                                                  |
+|-----|------------|--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+| 01  | [✓][day01] | [Trebuchet][aoc01]: Implemented the first part using a regex to find all digits in the string, then just taking the first and last element of the list. For the second part I continued down the regex path searching for the words. This however didn't work because of `1onenineqgzcq2eightwonh`... I couldn't find the last `two` with my regex fu. | 
+ 
+
+[aoc01]: https://adventofcode.com/2023/day/1
+[day01]: src/Day01.kt
+
 [aoc]: https://adventofcode.com
 [docs]: https://kotlinlang.org/docs/home.html
 [github]: https://github.com/tbilou
--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,41 @@
 fun main() {
+
+    fun getNumber(input:String):Int {
+        val pattern = "\\d".toRegex()
+        val result = pattern.findAll(input)
+        val numbers = result.map { it.value.toInt() }.toList()
+        val firstNumber = numbers.first()
+        val lastNumber = numbers.last()
+        return "$firstNumber$lastNumber".toInt()
+    }
+
+    fun getNumberfromWord(line:String):Int{
+        val map = hashMapOf("one" to 1, "two" to 2, "three" to 3, "four" to 4, "five" to 5, "six" to 6, "seven" to 7, "eight" to 8, "nine" to 9)
+
+        val firstDigit = line.findAnyOf(listOf("1", "2", "3", "4", "5", "6", "7", "8", "9")) ?: (Int.MAX_VALUE to "")
+        val lastDigit = line.findLastAnyOf(listOf("1", "2", "3", "4", "5", "6", "7", "8", "9")) ?: (Int.MIN_VALUE to "")
+        val firstWord = line.findAnyOf(listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")) ?: (Int.MAX_VALUE to "")
+        val lastWord = line.findLastAnyOf(listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")) ?: (Int.MIN_VALUE to "")
+
+        val first = if (firstDigit.first < firstWord.first) firstDigit.second else map[firstWord.second]
+        val last = if (lastDigit.first > lastWord.first) lastDigit.second else map[lastWord.second]
+
+        return "$first$last".toInt()
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.fold(0) { acc, i -> acc + getNumber(i) }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.fold(0) { acc, i -> acc + getNumberfromWord(i) }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
+    val testInput2 = readInput("Day01_2_test")
+    check(part2(testInput2) == 293)
 
     val input = readInput("Day01")
     part1(input).println()
