388e4a04a4efcd0fa17070450a52744cb1f16063
tiago bilou <tiago.bilou@xing.com>
2023-12-08 15:35:18 +0100

    first part works. Second part doesnt'

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,93 +1,131 @@
+import kotlin.system.measureTimeMillis
+
 fun main() {
 
-    val seed2soil:MutableMap<Long, Long> = mutableMapOf()
-    val soil2fertilizer:MutableMap<Long, Long> = mutableMapOf()
-    val fertilizer2water:MutableMap<Long, Long> = mutableMapOf()
-    val water2light:MutableMap<Long, Long> = mutableMapOf()
-    val light2temperature:MutableMap<Long, Long> = mutableMapOf()
-    val temperature2humidity:MutableMap<Long, Long> = mutableMapOf()
-    val humidity2location:MutableMap<Long, Long> = mutableMapOf()
+    var seed2soil: List<Banana> = listOf()
+    var soil2fertilizer: List<Banana> = listOf()
+    var fertilizer2water: List<Banana> = listOf()
+    var water2light: List<Banana> = listOf()
+    var light2temperature: List<Banana> = listOf()
+    var temperature2humidity: List<Banana> = listOf()
+    var humidity2location: List<Banana> = listOf()
 
 
     fun getLocationForSeed(seed: Long): Long {
-        val soil = seed2soil.getOrDefault(seed, seed)
-        val fertilizer = soil2fertilizer.getOrDefault(soil, soil)
-        val water = fertilizer2water.getOrDefault(fertilizer, fertilizer)
-        val light = water2light.getOrDefault(water, water)
-        val temp = light2temperature.getOrDefault(light, light)
-        val humidity = temperature2humidity.getOrDefault(temp, temp)
-        val location = humidity2location.getOrDefault(humidity, humidity)
-        return location
+//        println("Seed number: $seed")
+        var soil = seed2soil.map { b -> b.destination(seed) }
+            .filter { (it != null) }
+            .elementAtOrElse(0) { seed }
+        val fertilizer = soil2fertilizer.map { b -> b.destination(soil!!) }
+            .filter { (it != null) }
+            .elementAtOrElse(0) { soil }
+        val water = fertilizer2water.map { b -> b.destination(fertilizer!!) }
+            .filter { (it != null) }
+            .elementAtOrElse(0) { fertilizer }
+        val light = water2light.map { b -> b.destination(water!!) }
+            .filter { (it != null) }
+            .elementAtOrElse(0) { water }
+        val temp = light2temperature.map { b -> b.destination(light!!) }
+            .filter { (it != null) }
+            .elementAtOrElse(0) { light }
+        val humidity = temperature2humidity.map { b -> b.destination(temp!!) }
+            .filter { (it != null) }
+            .elementAtOrElse(0) { temp }
+        val location = humidity2location.map { b -> b.destination(humidity!!) }
+            .filter { (it != null) }
+            .elementAtOrElse(0) { humidity }
+        return location!!
     }
+
+    fun calculateMaps(input: List<String>) {
+        // seed-to-soil
+        seed2soil = input.get(1).split("\n").drop(1)
+            .map { it.split(" ") }
+            .map { (d, s, l) -> Banana(d.toLong(), s.toLong(), l.toLong()) }
+
+        // soil-to-fertilizer
+        soil2fertilizer = input.get(2).split("\n").drop(1)
+            .map { it.split(" ") }
+            .map { (d, s, l) -> Banana(d.toLong(), s.toLong(), l.toLong()) }
+
+        // fertilizer-to-water
+        fertilizer2water = input.get(3).split("\n").drop(1)
+            .map { it.split(" ") }
+            .map { (d, s, l) -> Banana(d.toLong(), s.toLong(), l.toLong()) }
+
+        //water-to-light
+        water2light = input.get(4).split("\n").drop(1)
+            .map { it.split(" ") }
+            .map { (d, s, l) -> Banana(d.toLong(), s.toLong(), l.toLong()) }
+
+        //light-to-temperature
+        light2temperature = input.get(5).split("\n").drop(1)
+            .map { it.split(" ") }
+            .map { (d, s, l) -> Banana(d.toLong(), s.toLong(), l.toLong()) }
+
+        //temperature-to-humidity
+        temperature2humidity = input.get(6).split("\n").drop(1)
+            .map { it.split(" ") }
+            .map { (d, s, l) -> Banana(d.toLong(), s.toLong(), l.toLong()) }
+
+        //humidity-to-location
+        humidity2location = input.get(7).split("\n").drop(1)
+            .map { it.split(" ") }
+            .map { (d, s, l) -> Banana(d.toLong(), s.toLong(), l.toLong()) }
+    }
+
 
     fun part1(input: List<String>): Long {
 
+        measureTimeMillis { calculateMaps(input) }
+            .also { println("time to calculate maps: $it") }
 
 
-
-        val seeds:List<Long> = input.get(0).split(": ").drop(1)
+        val seeds: List<Long> = input.get(0).split(": ").drop(1)
             .flatMap { it.split(" ") }
-            .map {it.toLong()}
+            .map { it.toLong() }
 
-        // seed-to-soil
-        input.get(1).split("\n").drop(1)
-            .map{it.split(" ")}
-            .map { (d,s,l)-> Banana(d.toLong(),s.toLong(),l.toLong()) }
-            .map {b -> b.expandRange(seed2soil)}
+        val seed2location = seeds.map { seed ->
+            var location = 0L
+            measureTimeMillis {
+                location = getLocationForSeed(seed)
+            }.also { println("time to iterave over the maps $it") }
+            location
+        }
+        val min = seed2location.min()
 
-        // soil-to-fertilizer
-        input.get(2).split("\n").drop(1)
-            .map{it.split(" ")}
-            .map { (d,s,l)-> Banana(d.toLong(),s.toLong(),l.toLong()) }
-            .map {b -> b.expandRange(soil2fertilizer)}
-
-        // fertilizer-to-water
-        input.get(3).split("\n").drop(1)
-            .map{it.split(" ")}
-            .map { (d,s,l)-> Banana(d.toLong(),s.toLong(),l.toLong()) }
-            .map {b -> b.expandRange(fertilizer2water)}
-
-        //water-to-light
-        input.get(4).split("\n").drop(1)
-            .map{it.split(" ")}
-            .map { (d,s,l)-> Banana(d.toLong(),s.toLong(),l.toLong()) }
-            .map {b -> b.expandRange(water2light)}
-
-        //light-to-temperature
-        input.get(5).split("\n").drop(1)
-            .map{it.split(" ")}
-            .map { (d,s,l)-> Banana(d.toLong(),s.toLong(),l.toLong()) }
-            .map {b -> b.expandRange(light2temperature)}
-
-        //temperature-to-humidity
-        input.get(6).split("\n").drop(1)
-            .map{it.split(" ")}
-            .map { (d,s,l)-> Banana(d.toLong(),s.toLong(),l.toLong()) }
-            .map {b -> b.expandRange(temperature2humidity)}
-
-        //humidity-to-location
-        input.get(7).split("\n").drop(1)
-            .map{it.split(" ")}
-            .map { (d,s,l)-> Banana(d.toLong(),s.toLong(),l.toLong()) }
-            .map {b -> b.expandRange(humidity2location)}
-
-//       seeds.sumOf { seed ->
-        val total = seeds.map { seed -> getLocationForSeed(seed) }.min()
-        return total
+        return min
     }
 
 
-    fun part2(input: List<String>): Int {
-        return input.count()
+    fun part2(input: List<String>): Long {
+        calculateMaps(input)
+
+        val seedRanges = input.get(0).split(": ").drop(1)
+            .flatMap { it.split(" ") }
+            .map { it.toLong() }
+            .windowed(2, 2, false)
+        var results:MutableList<Long> = mutableListOf()
+
+        for (seedRange in seedRanges)
+        {
+            results.add( (seedRange.first()..seedRange.first()+seedRange.last()-1).toSet()
+//                .also { println(it) }
+                .map { seed -> getLocationForSeed(seed) }
+                .min()
+            )
+        }
+
+        return results.min()
     }
 
 // test if implementation meets criteria from the description, like:
     val testInput = readInput2("Day05_test")
     check(part1(testInput) == 35L)
-//    val testInput2 = readInput("Day05_test")
-//    check(part2(testInput2) == 30)
+    val testInput2 = readInput2("Day05_test")
+    check(part2(testInput2) == 46L)
 
-//    val input = readInput("Day05")
+    val input = readInput2("Day05")
 //    part1(input).println()
 //    part2(input).println()
 }
@@ -97,9 +135,15 @@ data class Banana(
     val sourceRangeStart: Long,
     val rangeLength: Long
 ) {
-    fun expandRange(map: MutableMap<Long, Long>) {
-        for (i in 0..rangeLength-1){
-            map[sourceRangeStart+i] = destinationRangeStart+i
+//    private val range: LongRange = sourceRangeStart..<sourceRangeStart + rangeLength -1
+    fun destination(source: Long): Long? {
+        val sourceRange = sourceRangeStart..sourceRangeStart + rangeLength - 1
+        val contains = sourceRange.contains(source)
+        if (contains) {
+            val index = sourceRange.indexOf(source)
+            val destination = destinationRangeStart + index
+            return destination
         }
+        return null
     }
 }
